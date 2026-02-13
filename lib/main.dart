2de8a201276f0414ee023b8d4faecc9ca24e90a3
giddy-e23 <gideonryan14@gmail.com>
2026-02-13 18:05:13 +0300

    chore: remove unused imports from main.dart
    - Remove iconsax_plus, welcome, widgets_page, showcase_page imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:iconsax_plus/iconsax_plus.dart';
-import 'package:task_todo_app/feature/auth/presentation/pages/welcome.dart';
 import 'package:task_todo_app/feature/home/nav_page.dart';
-import 'package:task_todo_app/feature/widgets_page.dart';
 import 'core/theme/theme.dart';
-import 'feature/showcase/presentation/pages/showcase_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
